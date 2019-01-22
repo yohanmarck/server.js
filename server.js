ab53cd3296ce20f06365044a46629e3af80c3cbf
yohan M <yohan.marck@eemi.com>
2019-01-22 18:04:15 +0100

    ajout /chat

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/hello', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/chat', function (req, res) {
+    res.send('Hello World!')
+  })
+
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!')
 })
